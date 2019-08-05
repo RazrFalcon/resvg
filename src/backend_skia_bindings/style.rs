e90bdc1b59c62b6529c3ee885fc87be7de8ad0c4
Velislava Yanchina <vel.yanchina@yahoo.com>
2019-08-05 15:23:08 +1000

    style bindings

--- src/backend_skia_bindings/style.rs
+++ src/backend_skia_bindings/style.rs
@@ -27,23 +27,22 @@ pub fn fill(
                 let color = skia::Color::from_argb(a, c.red, c.green, c.blue);
                 paint.set_color(color);
             },
-            _ => {}
-            // usvg::Paint::Link(ref id) => {
-            //     if let Some(node) = tree.defs_by_id(id) {
-            //         match *node.borrow() {
-            //             usvg::NodeKind::LinearGradient(ref lg) => {
-            //                 prepare_linear(lg, opacity, bbox, &mut paint);
-            //             }
-            //             usvg::NodeKind::RadialGradient(ref rg) => {
-            //                 prepare_radial(rg, opacity, bbox, &mut paint);
-            //             }
-            //             usvg::NodeKind::Pattern(ref pattern) => {
-            //                 prepare_pattern(&node, pattern, opt, global_ts, bbox, opacity, &mut paint);
-            //             }
-            //             _ => {}
-            //         }
-            //     }
-            // }
+            usvg::Paint::Link(ref id) => {
+                if let Some(node) = tree.defs_by_id(id) {
+                    match *node.borrow() {
+                        usvg::NodeKind::LinearGradient(ref lg) => {
+                            prepare_linear(lg, opacity, bbox, &mut paint);
+                        }
+                        usvg::NodeKind::RadialGradient(ref rg) => {
+                            prepare_radial(rg, opacity, bbox, &mut paint);
+                        }
+                        usvg::NodeKind::Pattern(ref pattern) => {
+                            prepare_pattern(&node, pattern, opt, global_ts, bbox, opacity, &mut paint);
+                        }
+                        _ => {}
+                    }
+                }
+            },
         }
     }
 
@@ -68,23 +67,22 @@ pub fn stroke(
                 let color = skia::Color::from_argb(a, c.red, c.green, c.blue);
                 paint.set_color(color);
             },
-            _ => {},
-            // usvg::Paint::Link(ref id) => {
-            //     if let Some(node) = tree.defs_by_id(id) {
-            //         match *node.borrow() {
-            //             usvg::NodeKind::LinearGradient(ref lg) => {
-            //                 prepare_linear(lg, opacity, bbox, &mut paint);
-            //             }
-            //             usvg::NodeKind::RadialGradient(ref rg) => {
-            //                 prepare_radial(rg, opacity, bbox, &mut paint);
-            //             }
-            //             usvg::NodeKind::Pattern(ref pattern) => {
-            //                 prepare_pattern(&node, pattern, opt, global_ts, bbox, opacity, &mut paint);
-            //             }
-            //             _ => {}
-            //         }
-            //     }
-            // }
+            usvg::Paint::Link(ref id) => {
+                if let Some(node) = tree.defs_by_id(id) {
+                    match *node.borrow() {
+                        usvg::NodeKind::LinearGradient(ref lg) => {
+                            prepare_linear(lg, opacity, bbox, &mut paint);
+                        }
+                        usvg::NodeKind::RadialGradient(ref rg) => {
+                            prepare_radial(rg, opacity, bbox, &mut paint);
+                        }
+                        usvg::NodeKind::Pattern(ref pattern) => {
+                            prepare_pattern(&node, pattern, opt, global_ts, bbox, opacity, &mut paint);
+                        }
+                        _ => {}
+                    }
+                }
+            },
         }
 
         let stroke_cap = match stroke.linecap {
@@ -104,132 +102,161 @@ pub fn stroke(
         paint.set_stroke_miter(stroke.miterlimit.value() as f32);
         paint.set_stroke_width(stroke.width.value() as f32);
 
-        // if let Some(ref list) = stroke.dasharray {
-        //     let list: Vec<_> = list.iter().map(|n| *n as f32).collect();
-        //     let path_effect = skia::PathEffect::new_dash_path(&list, stroke.dashoffset);
-        //     paint.set_path_effect(path_effect);
-        // }
+        if let Some(ref list) = stroke.dasharray {
+            let list: Vec<_> = list.iter().map(|n| *n as f32).collect();
+
+            let path_effect = skia::dash_path_effect::new(&list, stroke.dashoffset);
+            paint.set_path_effect(&path_effect);
+        }
     }
 
     paint
 }
 
-// fn prepare_linear(
-//     g: &usvg::LinearGradient,
-//     opacity: usvg::Opacity,
-//     bbox: Rect,
-//     paint: &mut skia::Paint,
-// ) {
-//     let gradient = skia::LinearGradient {
-//         start_point: (g.x1, g.y1),
-//         end_point: (g.x2, g.y2),
-//         base: prepare_base_gradient(g, opacity, &bbox)
-//     };
-//     let shader = skia::LinearGradient
-//     // let shader = skia::Shader::new_linear_gradient(gradient);
-//     paint.set_shader(&shader);
-// }
+fn prepare_linear(
+    g: &usvg::LinearGradient,
+    opacity: usvg::Opacity,
+    bbox: Rect,
+    paint: &mut skia::Paint,
+) {
+    let start_point = skia::Point{x: g.x1 as f32, y: g.y1 as f32};
+    let end_point = skia::Point{x: g.x2 as f32, y: g.y2 as f32};
+    let base_gradient = prepare_base_gradient(g, opacity, &bbox);
 
-// fn prepare_radial(
-//     g: &usvg::RadialGradient,
-//     opacity: usvg::Opacity,
-//     bbox: Rect,
-//     paint: &mut skia::Paint,
-// ) {
+    let shader = skia::Shader::linear_gradient(
+        (start_point, end_point),
+        base_gradient.colors.as_slice(),
+        Some(base_gradient.positions.as_slice()),
+        base_gradient.tile_mode,
+        None,
+        Some(&base_gradient.matrix),
+    );
+    paint.set_shader(&shader);
+}
 
-//     let gradient = skia::RadialGradient {
-//         start_circle: (g.fx, g.fy, 0.0),
-//         end_circle: (g.cx, g.cy, g.r.value()),
-//         base: prepare_base_gradient(g, opacity, &bbox)
-//     };
+fn prepare_radial(
+    g: &usvg::RadialGradient,
+    opacity: usvg::Opacity,
+    bbox: Rect,
+    paint: &mut skia::Paint,
+) {
+    let start_point = skia::Point{x: g.fx as f32, y: g.fy as f32};
+    let end_point = skia::Point{x: g.cx as f32, y: g.cy as f32};
+    let base_gradient = prepare_base_gradient(g, opacity, &bbox);
 
-//     let shader = skia::Shader::new_radial_gradient(gradient);
-//     paint.set_shader(&shader);
-// }
+    let shader = skia::Shader::two_point_conical_gradient(
+        start_point,
+        0.0,
+        end_point,
+        g.r.value() as f32,
+        base_gradient.colors.as_slice(),
+        Some(base_gradient.positions.as_slice()),
+        base_gradient.tile_mode,
+        None,
+        Some(&base_gradient.matrix),
+    );
 
-// fn prepare_base_gradient(
-//     g: &usvg::BaseGradient,
-//     opacity: usvg::Opacity,
-//     bbox: &Rect
-// ) -> skia::Gradient {
+    paint.set_shader(&shader);
+}
 
-//     let tile_mode = match g.spread_method {
-//         usvg::SpreadMethod::Pad => skia::TileMode::Clamp,
-//         usvg::SpreadMethod::Reflect => skia::TileMode::Mirror,
-//         usvg::SpreadMethod::Repeat => skia::TileMode::Repeat,
-//     };
+fn prepare_base_gradient(
+    g: & usvg::BaseGradient,
+    opacity: usvg::Opacity,
+    bbox: &Rect
+) -> Gradient {
+    let tile_mode = match g.spread_method {
+        usvg::SpreadMethod::Pad => skia::TileMode::Clamp,
+        usvg::SpreadMethod::Reflect => skia::TileMode::Mirror,
+        usvg::SpreadMethod::Repeat => skia::TileMode::Repeat,
+    };
+    let matrix = {
+            if g.units == usvg::Units::ObjectBoundingBox {
+                let mut ts = usvg::Transform::from_bbox(*bbox);
+                ts.append(&g.transform);
+                ts.to_native()
+            } else {
+                g.transform.to_native()
+            }
+    };
 
-//     let matrix = {
-//         if g.units == usvg::Units::ObjectBoundingBox {
-//             let mut ts = usvg::Transform::from_bbox(*bbox);
-//             ts.append(&g.transform);
-//             ts.to_native()
-//         } else {
-//             g.transform.to_native()
-//         }
-//     };
+    let mut colors: Vec<skia::Color> = Vec::new();
+    let mut positions: Vec<f32> = Vec::new();
 
-//     let mut colors: Vec<u32> = Vec::new();
-//     let mut positions: Vec<f32> = Vec::new();
+    for stop in &g.stops {
+        let a = (stop.opacity.value() * opacity.value() * 255.0) as u8;
+        let color = skia::Color::from_argb(a, stop.color.red, stop.color.green, stop.color.blue);
+        colors.push(color);
+        positions.push(stop.offset.value() as f32);
+    }
 
-//     for stop in &g.stops {
-//         let a = (stop.opacity.value() * opacity.value() * 255.0) as u8;
-//         let color = skia::Color::new(a, stop.color.red, stop.color.green, stop.color.blue);
-//         colors.push(color.to_u32());
-//         positions.push(stop.offset.value() as f32);
-//     }
+    Gradient {
+        colors,
+        tile_mode,
+        positions,
+        matrix,
+    }
+}
 
-//     skia::Gradient { colors, positions, tile_mode, matrix }
-// }
+fn prepare_pattern(
+    pattern_node: &usvg::Node,
+    pattern: &usvg::Pattern,
+    opt: &Options,
+    global_ts: usvg::Transform,
+    bbox: Rect,
+    opacity: usvg::Opacity,
+    paint: &mut skia::Paint,
+) {
+    let r = if pattern.units == usvg::Units::ObjectBoundingBox {
+        pattern.rect.bbox_transform(bbox)
+    } else {
+        pattern.rect
+    };
 
-// fn prepare_pattern(
-//     pattern_node: &usvg::Node,
-//     pattern: &usvg::Pattern,
-//     opt: &Options,
-//     global_ts: usvg::Transform,
-//     bbox: Rect,
-//     opacity: usvg::Opacity,
-//     paint: &mut skia::Paint,
-// ) {
-//     let r = if pattern.units == usvg::Units::ObjectBoundingBox {
-//         pattern.rect.bbox_transform(bbox)
-//     } else {
-//         pattern.rect
-//     };
+    let (sx, sy) = global_ts.get_scale();
 
-//     let (sx, sy) = global_ts.get_scale();
+    let img_size = try_opt!(Size::new(r.width() * sx, r.height() * sy)).to_screen_size();
+    let mut surface =  try_create_surface!(img_size, ());
+    let mut canvas = surface.canvas();
+    canvas.clear(skia::Color::TRANSPARENT);
 
-//     let img_size = try_opt!(Size::new(r.width() * sx, r.height() * sy)).to_screen_size();
-//     let mut surface =  try_create_surface!(img_size, ());
-//     let mut canvas = surface.canvas_mut();
-//     canvas.clear();
+    canvas.scale((sx as f32, sy as f32));
+    if let Some(vbox) = pattern.view_box {
+        let ts = utils::view_box_to_transform(vbox.rect, vbox.aspect, r.size());
+        canvas.concat(&ts.to_native());
+    } else if pattern.content_units == usvg::Units::ObjectBoundingBox {
+        // 'Note that this attribute has no effect if attribute `viewBox` is specified.'
 
-//     canvas.scale(sx, sy);
-//     if let Some(vbox) = pattern.view_box {
-//         let ts = utils::view_box_to_transform(vbox.rect, vbox.aspect, r.size());
-//         canvas.concat(&ts.to_native());
-//     } else if pattern.content_units == usvg::Units::ObjectBoundingBox {
-//         // 'Note that this attribute has no effect if attribute `viewBox` is specified.'
+        // We don't use Transform::from_bbox(bbox) because `x` and `y` should be
+        // ignored for some reasons...
+        canvas.scale((bbox.width() as f32, bbox.height() as f32));
+    }
 
-//         // We don't use Transform::from_bbox(bbox) because `x` and `y` should be
-//         // ignored for some reasons...
-//         canvas.scale(bbox.width(), bbox.height());
-//     }
+    let ref tree = pattern_node.tree();
+    let mut render = SkiaFlatRender::new(tree, opt, img_size, &mut surface);
+    render.render_group(pattern_node);
+    render.finish();
 
-//     let ref tree = pattern_node.tree();
-//     let mut render = SkiaFlatRender::new(tree, opt, img_size, &mut surface);
-//     render.render_group(pattern_node);
-//     render.finish();
+    let mut ts = usvg::Transform::default();
+    ts.append(&pattern.transform);
+    ts.translate(r.x(), r.y());
+    ts.scale(1.0 / sx, 1.0 / sy);
+    let image = surface.image_snapshot();
+    let shader = image.to_shader(
+        Some((skia::TileMode::Repeat, skia::TileMode::Repeat)),
+        Some(&ts.to_native()),
+    );
 
-//     let mut ts = usvg::Transform::default();
-//     ts.append(&pattern.transform);
-//     ts.translate(r.x(), r.y());
-//     ts.scale(1.0 / sx, 1.0 / sy);
-//     let shader = skia::Shader::new_from_surface_image(&surface, ts.to_native());
-//     paint.set_shader(&shader);
+    paint.set_shader(&shader);
 
-//     if !opacity.is_default() {
-//         let a = f64_bound(0.0, opacity.value() * 255.0, 255.0) as u8;
-//         paint.set_alpha(a);
-//     };
-// }
+    if !opacity.is_default() {
+        let a = f64_bound(0.0, opacity.value() * 255.0, 255.0) as u8;
+        paint.set_alpha(a);
+    };
+}
+
+struct Gradient {
+    colors: Vec<skia::Color>,
+    tile_mode: skia::TileMode,
+    positions: Vec<f32>,
+    matrix: skia::Matrix,
+}
