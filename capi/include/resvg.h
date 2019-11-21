f80ddb91c392cc78e27be7152b0d371f26f63aa2
Andrew Bennett <andrew@canva.com>
2019-11-21 11:49:52 +1100

    Update docs

--- capi/include/resvg.h
+++ capi/include/resvg.h
@@ -506,7 +506,7 @@ int resvg_skia_render_to_image(const resvg_render_tree *tree,
  * @param tree Render tree.
  * @param opt Rendering options.
  * @param size Canvas size.
- * @param surface Skia Surface.
+ * @param canvas Skia Canvas.
  */
 void resvg_skia_render_to_canvas(const resvg_render_tree *tree,
                                  const resvg_options *opt,
@@ -522,7 +522,7 @@ void resvg_skia_render_to_canvas(const resvg_render_tree *tree,
  * @param opt Rendering options.
  * @param size Canvas size.
  * @param id Node's ID.
- * @param surface Skia Surface.
+ * @param canvas Skia Canvas.
  */
 void resvg_skia_render_to_canvas_by_id(const resvg_render_tree *tree,
                                        const resvg_options *opt,
