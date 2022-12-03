95bef32d1823cb6999dbbfaeb0276af291dc5618
Yevhenii Reizner <razrfalcon@gmail.com>
2022-12-03 21:10:33 +0200

    Fix win build.

--- tools/explorer-thumbnailer/src/utils.rs
+++ tools/explorer-thumbnailer/src/utils.rs
@@ -6,30 +6,35 @@ use winapi::shared::windef::{HBITMAP, HDC};
 use winapi::um::objidlbase::{LPSTREAM, STATSTG};
 use winapi::um::wingdi::{BI_RGB, BITMAPINFO, BITMAPINFOHEADER, DIB_RGB_COLORS, CreateDIBSection};
 use com::sys::S_OK;
-use resvg::{usvg, tiny_skia};
+use resvg::{usvg, tiny_skia, usvg_text_layout};
 use usvg::FitTo;
+use usvg_text_layout::{fontdb, TreeTextToPath};
 use crate::error::Error;
 
 pub unsafe fn tree_from_istream(pstream: LPSTREAM) -> Result<usvg::Tree, Error> {
     let mut stat: STATSTG = Default::default();
-        let stat_res = (*pstream).Stat(&mut stat, 0);
-        if stat_res != S_OK {
-            return Err(Error::IStreamStat(stat_res));
-        }
+    let stat_res = (*pstream).Stat(&mut stat, 0);
+    if stat_res != S_OK {
+        return Err(Error::IStreamStat(stat_res));
+    }
 
-        let size = *stat.cbSize.QuadPart();
-        let mut svg_data = Vec::with_capacity(size as usize);
-        let mut len: ULONG = 0;
-        let read_res = (*pstream).Read(svg_data.as_mut_ptr() as *mut c_void, size as u32, &mut len);
-        if read_res != S_OK {
-            return Err(Error::IStreamRead(read_res));
-        }
-        svg_data.set_len(len as usize);
+    let size = *stat.cbSize.QuadPart();
+    let mut svg_data = Vec::with_capacity(size as usize);
+    let mut len: ULONG = 0;
+    let read_res = (*pstream).Read(svg_data.as_mut_ptr() as *mut c_void, size as u32, &mut len);
+    if read_res != S_OK {
+        return Err(Error::IStreamRead(read_res));
+    }
+    svg_data.set_len(len as usize);
 
-        let mut opt = usvg::Options::default();
-        opt.fontdb.load_system_fonts();
+    let opt = usvg::Options::default();
 
-        usvg::Tree::from_data(&svg_data, &opt.to_ref()).map_err(|e| Error::TreeError(e))
+    let mut fontdb = fontdb::Database::new();
+    fontdb.load_system_fonts();
+
+    let mut tree = usvg::Tree::from_data(&svg_data, &opt).map_err(|e| Error::TreeError(e))?;
+    tree.convert_text(&fontdb, opt.keep_named_groups);
+    Ok(tree)
 }
 
 pub fn render_thumbnail(tree: &Option<usvg::Tree>, cx: u32) -> Result<tiny_skia::Pixmap, Error> {
