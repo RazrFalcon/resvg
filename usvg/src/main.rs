5e33ce7d4c6d68925c7d9d654228cedea93f2505
Felix Zwettler <f.zwettler@posteo.de>
2023-03-28 18:40:48 +0200

    fix fix

--- usvg/src/main.rs
+++ usvg/src/main.rs
@@ -273,7 +273,7 @@ fn parse_length(s: &str) -> Result<u32, String> {
 fn parse_precision(s: &str) -> Result<u8, String> {
     let n: u8 = s.parse().map_err(|_| "invalid precision NUM value")?;
 
-    if !(2..=8).contains(&n) {
+    if (2..=8).contains(&n) {
         Ok(n)
     } else {
         Err("precision NUM cannot be smaller than 2 or larger than 8".to_string())
