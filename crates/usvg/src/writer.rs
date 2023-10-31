13aadb7340d1944fc1d909eb397bd7f92300cdc7
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2023-10-31 17:41:24 +0100

    Add note about for loop

--- crates/usvg/src/writer.rs
+++ crates/usvg/src/writer.rs
@@ -592,9 +592,13 @@ fn conv_element(node: &Node, is_clip_path: bool, opt: &XmlOptions, xml: &mut Xml
         NodeKind::Group(ref g) => {
             if is_clip_path {
                 // ClipPath with a Group element is an `usvg` special case.
-                // Group will contain a sequence of path elements and we should set
+                // Group will contain a single path element and we should set
                 // `clip-path` on it.
 
+                // TODO: As mentioned above, if it is a group element it should only contain a
+                // single path element. However when converting text nodes to path, multiple
+                // paths might be present. As a temporary workaround, we just loop over all
+                // children instead.
                 for child in node.children() {
                     if let NodeKind::Path(ref path) = *child.borrow() {
                         let path = path.clone();
