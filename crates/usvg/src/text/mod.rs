74941060fed61dad8707b05274f34e535709a4c4
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2024-03-27 22:44:40 +0100

    Add comment

--- crates/usvg/src/text/mod.rs
+++ crates/usvg/src/text/mod.rs
@@ -9,15 +9,18 @@ mod outline;
 
 pub use layout::{PositionedGlyph, Span, TextFragment};
 
+/// Convert a text into its paths. This is done in two steps:
+/// 1. We convert the text into glyphs and position them according to the rules specified in the
+/// SVG specifiation. While doing so, we also calculate the text bbox (which is not based on the
+/// outlines of a glyph, but instead the glyph metrics as well as decoration spans).
+/// 2. We convert all of the positioned glyphs into outlines.
 pub(crate) fn convert(text: &mut Text, fontdb: &fontdb::Database) -> Option<()> {
     let (text_fragments, bbox) = layout::layout_text(text, fontdb)?;
-
     text.layouted = text_fragments;
     text.bounding_box = bbox.to_rect();
     text.abs_bounding_box = bbox.transform(text.abs_transform)?.to_rect();
 
     let (group, stroke_bbox) = outline::convert(text, fontdb)?;
-
     text.flattened = Box::new(group);
     text.stroke_bounding_box = stroke_bbox.to_rect();
     text.abs_stroke_bounding_box = stroke_bbox.transform(text.abs_transform)?.to_rect();
