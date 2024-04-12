f4289f7496d307ade3c6352bfce4c5148821b0b1
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2024-04-12 21:20:56 +0200

    Use ascent and descent insead of x-height for shift in tb writing mode.

--- crates/resvg/tests/integration/main.rs
+++ crates/resvg/tests/integration/main.rs
@@ -1,5 +1,6 @@
 use once_cell::sync::Lazy;
 use rgb::{FromSlice, RGBA8};
+use std::process::Command;
 use usvg::fontdb;
 
 #[rustfmt::skip]
@@ -55,6 +56,9 @@ pub fn render(name: &str) -> usize {
     resvg::render(&tree, render_ts, &mut pixmap.as_mut());
 
     // pixmap.save_png(&format!("tests/{}.png", name)).unwrap();
+    // Command::new("oxipng")
+    //     .args(["-o".to_owned(), "6".to_owned(), "-Z".to_owned(), format!("tests/{}.png", name)])
+    //     .output().unwrap();
 
     let mut rgba = pixmap.take();
     demultiply_alpha(rgba.as_mut_slice().as_rgba_mut());
