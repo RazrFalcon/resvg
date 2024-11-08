f476f3ba4b2c45ca1e80454d1ca24e31d0b97fe8
Kaur Kuut <strom@nevermore.ee>
2024-11-08 14:44:21 +0200

    Add cargo formatting.

--- tools/explorer-thumbnailer/src/interfaces/iinitialize_with_stream.rs
+++ tools/explorer-thumbnailer/src/interfaces/iinitialize_with_stream.rs
@@ -7,9 +7,5 @@ use winapi::um::objidlbase::LPSTREAM;
 
 #[com_interface("B824B49D-22AC-4161-AC8A-9916E8FA3F7F")]
 pub trait IInitializeWithStream: IUnknown {
-    unsafe fn read(
-        &self,
-        pstream: LPSTREAM,
-        grf_mode: DWORD
-    ) -> HRESULT;
+    unsafe fn read(&self, pstream: LPSTREAM, grf_mode: DWORD) -> HRESULT;
 }
